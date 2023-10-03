bebe14aa48d81615d5609eb48e82bcf36d89d0e4
S-varsha30 <105834229+S-varsha30@users.noreply.github.com>
2023-10-04 02:04:10 +0530

    Create Assignment 1.C

---
+++ Programming Assignments/Week 6/Assignment 1.C
@@ -0,0 +1,25 @@
+//Write a C Program to find Largest Element of an Integer Array.
+
+#include <stdio.h>
+
+int main()
+{
+    int i, n, largest;
+    int arr[100];
+
+    scanf("%d", &n); 
+
+ for(i = 0; i < n; ++i)
+    {
+       scanf("%d", &arr[i]);
+    }
+largest = arr[0];
+for(i = 1; i < n; ++i)
+    {
+           if(largest < arr[i])
+           largest = arr[i];
+    }
+    printf("Largest element = %d", largest);
+
+    return 0;
+}
