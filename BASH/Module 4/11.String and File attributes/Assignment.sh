282a5e8fc4fa4bb8011aee777bfd294371e0c2ab
S-varsha30 <105834229+S-varsha30@users.noreply.github.com>
2023-10-11 11:19:50 +0530

    Update Assignment.sh

--- BASH/Module 4/11.String and File attributes/Assignment.sh
+++ BASH/Module 4/11.String and File attributes/Assignment.sh
@@ -10,3 +10,15 @@ if [ -x "Logical_operators.sh" ]; then
 else 
    echo "Not an executable file"
 fi
+
+if [ -r "Logical_operators.sh" ]; then
+   echo "executable file"
+else 
+   echo "Not an executable file"
+fi
+
+if [ -o "Logical_operators.sh" ]; then
+    echo "File is owned by effective userid"
+else
+    echo "File is not owned by effective userid"
+fi
